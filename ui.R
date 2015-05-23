0e04863545c0707d37a68fa34bd25530b0902619
harishkrishnan <checkharish@gmail.com>
2015-05-23 18:10:58 +1200

    Changes made

--- ui.R
+++ ui.R
@@ -7,11 +7,18 @@ shinyUI(pageWithSidebar(
                 selected=names(iris)[[2]]),
     numericInput('clusters', 'Cluster count', 2,
                  min = 2, max = 9)
-  ),
+    
+      ),
   mainPanel(
     plotOutput('plot1'),
     
     h4("Number of observations from the Iris dataset per cluster:"),
-    verbatimTextOutput("summary")
+    verbatimTextOutput("summary"),
+    
+    h5("The above app helps one understand the concept of Heirarchical Clustering. 
+       Choose the X and Y vairable and the number of clusters on the left. The
+       Dendrogram on the right can be imagined as a tree and the observations based on the
+       similarities and dissimilarities are clustered into groups. The clustered observations
+       will have a lot commonalities relative to each other.")
       )
 ))
